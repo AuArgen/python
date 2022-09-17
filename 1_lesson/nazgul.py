b9fbeb4a4786b4bde50473f3debe18bb5a457d38
AuArgen <argenazizbekuulu3@gmail.com>
2022-09-17 22:17:19 +0600

    first

---
+++ 1_lesson/nazgul.py
@@ -0,0 +1,54 @@
+# a=input()
+# b=int(input())
+# print((a+"\n")*b)
+
+# a=input() + "\n"
+# b=int(input())
+# print(a*b)
+
+# a=input()
+# b=int(input())
+# a += "\n"
+# print(a*b)
+
+# a=input()
+# b=int(input())
+# a = a + "\n"
+# print(a*b)
+
+# print((input()+"\n") * int(input()))
+
+# count - bul s1 sabyndagy s2 sozu kancha iret kezdeshuusun anyktait
+
+# s1=input()
+# s2=input()
+# print(s1.count(s2))
+
+# len - bul symboldordun sanyn espteit
+
+# s = input()
+# a = len(s)
+# print(a)
+
+# find - bul s1 textindegi sodon s2 sozun tapsa s2 sozu s1 din kanchanchy symboldon kiyin ekenin taap beret
+
+# s1 = input()
+# s2 = input()
+# print(s1.find(s2)+1)
+
+# s1 = input()
+# print(s1[1::])
+
+# s1=input()
+# a=len(s1)
+# a=int(a/2)
+# print(s1[a::])
+
+# s1 = input()
+# print(s1[0:3])
+
+a=input()
+b=len(a)
+b=int(b/2)
+print(a[0:b])
+print(a[b::])
